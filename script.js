c96debba28726865c86bb392883e2e58c1fea7e8
Mohd Shadab <89182232+shadabibrahim@users.noreply.github.com>
2024-02-14 22:58:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,22 +4,29 @@ class Person {
 		this.name= name;
 		this.age = age;
 	}
-	getName(){
-		return this.name;
-	}
-	setAge(age){
-		this._age = age;
-		
-	}
+	get name() {
+    return this.name;
+  }
+
+	 set age(age) {
+    this.age = age;
+  }
 }
 
 class Student extends Person {
+	 constructor(name, age) {
+    super(name, age); // Call the parent constructor
+  }
 	study(){
 		console.log(`${this.name} is studying`)
 	}
 }
 
 class Teacher extends Person {
+	constructor(name, age) {
+    super(name, age); // Call the parent constructor
+  }
+
 	teach(){
 		console.log(`${this.name} is teaching`)
 	}
