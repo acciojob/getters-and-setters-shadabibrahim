735cf4fde4713b9ae05449f9f2889c4d019fad25
Mohd Shadab <89182232+shadabibrahim@users.noreply.github.com>
2024-02-15 23:54:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,47 +1,24 @@
-//complete this code
 class Person {
 	constructor(name,age){
-		this.name= name;
-		this.age = age;
+		this._name=name;
+		this._age=age;
 	}
-	get name() {
-    return this.name;
-  }
-
-	 set age(age) {
-    this.age = age;
-  }
+	get name(){ return this._name;}
+	set age(age){this._age=age;}
+	get age(){ return this._age;}
 }
 
 class Student extends Person {
-	 constructor(name, age) {
-    super(name, age); // Call the parent constructor
-  }
 	study(){
-		console.log(`${this.name} is studying`)
+		console.log(`${this._name} is studying` )
 	}
 }
 
 class Teacher extends Person {
-	constructor(name, age) {
-    super(name, age); // Call the parent constructor
-  }
-
 	teach(){
-		console.log(`${this.name} is teaching`)
-	}
-}
-const person = new Person("John", 25);
-console.log(person.name); // Output: John
+		console.log(`${this._name} is teaching` )
+	}}
 
-person.age = 30;
-console.log(person.age); // Output: 30
-
-const student = new Student("Alice", 22);
-student.study(); // Output: Alice is studying
-
-const teacher = new Teacher("Bob", 40);
-teacher.teach(); // Output: Bob is teaching
 // Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
